987878565636abaa1bb6edaf7f9277583cd68345
siutin <osiutino@gmail.com>
2024-11-09 17:48:10 +0800

    add instant view support

--- src/renderer.js
+++ src/renderer.js
@@ -7,10 +7,49 @@ export default function (data) {
         elements.push(renderImage(data))
     }
     elements.push(renderText(data))
+    elements.push(renderInstantView(data))
 
     const meta = elements.filter(element => element.metaArray).flatMap(element => element.metaArray).join('')
     const body = elements.filter(element => element.bodyArray).flatMap(element => element.bodyArray).join('')
     return `<html><head>${meta}</head><body>${body}</body></html>`
+}
+
+function renderStatus(data) {
+    return `<p>${data.likeCount} ❤️ ${data.replyCount} 💬 ${data.repostCount} 🔁 ${data.shareCount}</p>`
+}
+
+function renderInstantView(data) {
+    function generateMediaTag(data) {
+        if (data.hasVideo) {
+            return `<video src="${data.video.url}" controls></video>`
+        }
+        else if (data.hasImage) {
+            return `<img src="${data.image.url}" alt="${data.image.alt}" />`
+        }
+    }
+
+    return {
+        metaArray: [
+            `<meta property="al:android:app_name" content="Medium"/>`,
+            `<meta property="article:published_time" content="${data.createdAt}" />`
+        ],
+        bodyArray: [
+            `<section class="section-backgroundImage"><figure class="graf--layoutFillWidth"></figure></section>`,
+            `<article>
+                <sub><a href="${data.url}">View full thread</a></sub>
+
+                <sub>(${data.username})</sub>
+                <p>${data.description}</p>
+                ${generateMediaTag(data)}
+                ${renderStatus(data.status)}
+                
+                <h2>About author</h2>
+                <img src="${data.profileImageURL}" alt="${data.username}'s profile picture" />
+                <h2>${data.authorName}</h2>
+                <p><a href="https://www.threads.net/${data.username}">${data.username}</a></p>
+            </article>`
+        ]
+    }
 }
 
 function renderText(data) {
