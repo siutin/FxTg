f2649f4394917bea0ecd2b07a691013d67d5a52c
siutin <osiutino@gmail.com>
2024-12-05 18:10:45 +0800

    parser - capture a screenshot when error occurs

--- src/parser.js
+++ src/parser.js
@@ -56,6 +56,7 @@ export class Parser {
             }
         } catch (error) {
             logger.log('error', error, { stack: error?.stack })
+            await page.screenshot({path: `../public/pics/${new Date().toISOString().replace(/[\.\-T:Z]/g,'')}.png`, fullPage: true})
         } finally {
             await page.close()
             logger.log('debug', `[${this.constructor.name}] Page closed`)
